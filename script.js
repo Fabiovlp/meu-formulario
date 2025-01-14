3f30e9dc2cb419a305e5e6c1cc90140234b02852
Fabiovlp <fabiovlimap@gmail.com>
2025-01-14 12:19:25 -0300

    Update and rename spript.js to script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script>
         async function gerarPDF() {
             const { jsPDF } = window.jspdf;
